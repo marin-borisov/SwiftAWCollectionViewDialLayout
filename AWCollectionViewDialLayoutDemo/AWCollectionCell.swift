d7c70aee57cc5293b3f0fc9f868d27e4bcf08c19
Marin Borisov <marborisov@gmail.com>
2016-10-16 19:16:13 +0300

    first commit

---
+++ AWCollectionViewDialLayoutDemo/AWCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  AWCollectionCellCollectionViewCell.swift
+//  AWCollectionViewDialLayoutDemo
+//
+//  Created by Marin Borisov on 5/29/16.
+//  Copyright © 2016 Marin Borisov. All rights reserved.
+//
+
+import UIKit
+
+class AWCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var icon:UIImageView!
+    @IBOutlet weak var label:UILabel!
+    
+}
